b931093fb2880f15f9410593c7191bb41d6d15ae
Isaiah Odhner <isaiahodhner@gmail.com>
2022-02-18 01:05:19 -0500

    Set up eslint (and live-server)

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [
+			"error",
+			"tab"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
